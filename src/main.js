1befe6037ad54ca7e3b5c563daaeefd679aa0fd3
hayoungzzzzz <hayoung474@gmail.com>
2021-02-06 04:16:02 +0900

    test add chatbot

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import VueFbCustomerChat from "vue-fb-customer-chat";
+
+Vue.use(VueFbCustomerChat, {
+  page_id: 356413202061705, //  change 'null' to your Facebook Page ID,
+  theme_color: "#333333", // theme color in HEX
+  locale: "en_US", // default 'en_US'
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
