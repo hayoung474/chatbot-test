a2e45108de7f171ed35fa68c9b046790aaccfbe5
20160450 <olympiodoros@naver.com>
2021-02-06 15:34:47 +0900

    deploy

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueFbCustomerChat from "vue-fb-customer-chat";
 
 Vue.config.productionTip = false;
-import VueFbCustomerChat from "vue-fb-customer-chat";
 
 Vue.use(VueFbCustomerChat, {
   page_id: 356413202061705, //  change 'null' to your Facebook Page ID,
   theme_color: "#333333", // theme color in HEX
-  locale: "en_US", // default 'en_US'
+  locale: "ko_KR", // default 'en_US'
 });
 
 new Vue({
